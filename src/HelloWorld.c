6bdac98c9dd82856f075992e84d0050b26930f29
Josh Farias <joshfarias.github@gmail.com>
2023-04-15 18:44:47 -0400

    Hello World
    Hello World

---
+++ src/HelloWorld.c
@@ -0,0 +1,22 @@
+/************************************************************
+|
+|   Program Name: HelloWorld.c
+|
+|   Description: Hello World
+|
+|   Language: C
+|
+|   Date: 12/3/2022
+|
+|   Author: Joshua Farias
+|
+************************************************************/
+
+//include standard input/output library
+#include <stdio.h>
+
+//main function
+int main() {
+    printf("Hello, World!"); //print text
+    return 0; //end of program
+}
